331e2304ee820305ac8e4374becd94c3a69ff99f
Doug Beney <work@dougie.io>
2018-01-05 21:40:48 -0500

    domain quality

--- domain_quality_filter.py
+++ domain_quality_filter.py
@@ -1,20 +1,21 @@
 from dougsheets import plugin
 
-bad_patterns = [
-    "blogspot",
-    "wordpress.com",
-    "weebly.com",
-    "wix.com",
-    ".shopify.com"
-]
 
-def contains_bad_pattern(string):
-    for pattern in bad_patterns:
-        if pattern in str(string):
-            return True
-    return False
+class DomainQuality(plugin.Plugin):
+    bad_patterns = [
+        "blogspot",
+        "wordpress.com",
+        "weebly.com",
+        "wix.com",
+        ".shopify.com"
+    ]
 
-class MyPlugin(plugin.Plugin):
+    def contains_bad_pattern(self, string):
+        for pattern in self.bad_patterns:
+            if pattern in str(string):
+                return True
+        return False
+
     def domainQualityAction(self, e):
         userinput = self.getDialog("Domain Quality Filter", "What column holds your URLS?")
         if userinput:
@@ -26,25 +27,19 @@ class MyPlugin(plugin.Plugin):
 
                 sheetObject = self.gui.sheetObject
 
-                for col in sheetObject:
-                    newSheetObject.append(
-                        [str(col[0]), []]
-                    )
-                counter = 0
-                for url in sheetObject[url_col][1]:
-                    if not contains_bad_pattern(url):
-                        for i in range(0, len(newSheetObject)):
-                            newSheetObject[i][1].append(sheetObject[i][1][counter])
-                    counter += 1
+                for row in sheetObject:
+                    if not self.contains_bad_pattern(row[url_col]):
+                        newSheetObject.append(row)
 
                 self.gui.sheetObject = newSheetObject.copy()
                 self.gui.update_sheet()
 
     def init(self):
-        gui = self.gui
-        self.createMenuItem(gui.menu_filter, "Domain Quality", self.domainQualityAction)
+        self.createMenuItem(self.gui.menu_filter, "Domain Quality", self.domainQualityAction)
 
 settings = {
-    "class": MyPlugin
+    "class": DomainQuality,
+    'name': "Domain Quality Filter",
+    'version': "0.0.1",
 }
 
